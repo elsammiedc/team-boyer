4221346a608ae99677b0daecb79af81a4d95ab31
Gracious <59148031+grapemw@users.noreply.github.com>
2020-08-02 23:30:38 +0200

    Add files via upload

---
+++ @grape.py
@@ -0,0 +1,8 @@
+def personal_id():
+    print("Name : ""Gracious Mwandira")
+    print("Email : ""grapemw@gmail.com")
+    print("Language : ""python")
+    print ("Biostack : ""Genomics")
+    print("Username : ""@grape")
+
+personal_id()
