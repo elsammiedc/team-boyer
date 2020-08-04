df3add1b676d4cca882bdbab4fccec60831ec79e
walili <waliu.jimoh@yahoo.com>
2020-08-04 19:48:11 +0100

    Create @_drjimoh.js

---
+++ @_drjimoh.js
@@ -0,0 +1,10 @@
+function task ( ) {
+var name = "Jimoh Waliu Ololade";
+var email = "itsmartdigital@gmail.com";
+var language = "JavaScript";
+var biostack = "PROTEOMICS";
+var username = "@_drjimoh";
+
+console.log( "Name: ", name, " email: ", email, " language: ", language, " biostack: ", biostack, " username: " , username) 
+
+};
