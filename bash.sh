094883493fef34013f17d5a5a67a41f6c967f4f2
Gracious Mwandira <grapemw@gmail.com>
2020-08-06 00:31:47 +0200

    more scripts

---
+++ bash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo Name: Gracious
+echo Email: grapemw@gmail.com
+echo Language: Bash
+echo Biostack: Genomics
+echo Slack_name: @grape
---
+++ java.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.OutputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Name: Gracious Mwandira");
+        System.out.println("Email: grapemw@gmail.com");
+        System.out.println("Language: java");
+        System.out.println("Biostack: Genomics");
+        System.out.println("Slack_Username: @grape");
+    }
+}
