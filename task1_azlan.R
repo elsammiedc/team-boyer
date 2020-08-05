25f26155f321cd88150410e3aeeb6e57be2545ef
IamAzlan <69126060+IamAzlan@users.noreply.github.com>
2020-08-05 01:33:07 +0100

    Add files via upload

---
+++ task1_azlan.R
@@ -0,0 +1,15 @@
+str1 <- "Name: Abdul-Azeez Laniihun"
+print(str1)
+str2 <- "Email: lanihun20@gmail.com"
+print(str2)
+str3 <- "Language: R"
+print(str3)
+str4 <- "biostack: Drug Discovery and Genomics"
+print(str4)
+str5 <- "Username: @Azlan"
+print(str5)
+paste(str1,
+      str2,
+      str3,
+      str4,
+      str5)
