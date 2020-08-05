9fa850416e21e76bf1c5f364281b0a17fffec25b
Ibironke Sanni <ronke.sanni03@gmail.com>
2020-08-05 01:11:04 -0400

    This is the Perl Script of task 1

---
+++ HackBio Stage 0/Assignment1.pl
@@ -0,0 +1,28 @@
+#! /usr/bin/perl
+use warnings;
+use strict;
+use feature qw(say);
+
+################################################################################################
+#
+#	File    :  Assignment1.pl
+#	History :  05-August-2020 (Ibi) sat down to code up task 1
+#
+################################################################################################
+
+
+my $name = "Ibironke Sanni";
+say "Hello, my name is $name.\n";
+
+my $email = "Ibironke_sanni\@yahoo.com";
+say "My email address is $email.\n";
+
+my $language = "Perl";
+say "I am currently using $language.\n";
+
+my $biostack = "Drug Discovery and Development";
+say "My biostack is $biostack.\n";
+
+my $slack_uname = " \@SimplyIbi ";
+say "My Slack Username is $slack_uname.\n";
+
