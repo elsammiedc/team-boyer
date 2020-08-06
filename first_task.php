bcf9e3fbaa87680f186166fdb8d03a83c037a346
Ibironke Sanni <ronke.sanni03@gmail.com>
2020-08-06 11:17:20 -0400

    Php code

---
+++ first_task.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+      $myName = "Ibironke Sanni";
+      echo "My name is $myName <br>";
+
+      $myEmail = "Ibironke_sanni@yahoo.com";
+      echo "My email is $myEmail <br>";
+
+      $language = "php";
+      echo "The scripting language is $language <br>";
+
+      $mybiostack = "Drug Devlopment and Genomics";
+      echo "My biostack is $mybiostack <br>";
+
+      $slack_usrname = "@simplyibi";
+      echo "My slack username is $slack_usrname <br>";
+     ?>
+
+  </body>
+</html>
