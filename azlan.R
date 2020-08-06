6936d69fd4e3dd7527ce782b38e457a9a9127062
Gracious <59148031+grapemw@users.noreply.github.com>
2020-08-06 09:59:59 +0200

    Update azlan.R

--- azlan.R
+++ azlan.R
@@ -1,9 +1,10 @@
-Name<- "Abdul-Azeez Laniihun"
-print(Name)
-Email<- "lanihun20@gmail.com"
-print(Email)
-Language<- "R"
-print(Language)
-Biostack<- "Drug discovery"
-print(Biostack)
-Username<- "@Azlan"
+str1 <- "Abdul-Azeez Laniihun , "
+str2 <- "lanihun20@gmail.com ,"
+str3 <- "R ,"
+str4 <- "Drug Discovery and Genomics ,"
+str5 <- "@Azlan"
+paste(str1,
+      str2,
+      str3,
+      str4,
+      str5)
