c201dec9e2810b01b4785b647e22232c163e7153
Gracious <59148031+grapemw@users.noreply.github.com>
2020-08-06 09:43:11 +0200

    Update and rename task1_azlan.R to azlan.R

---
+++ azlan.R
@@ -0,0 +1,9 @@
+Name<- "Abdul-Azeez Laniihun"
+print(Name)
+Email<- "lanihun20@gmail.com"
+print(Email)
+Language<- "R"
+print(Language)
+Biostack<- "Drug discovery"
+print(Biostack)
+Username<- "@Azlan"
